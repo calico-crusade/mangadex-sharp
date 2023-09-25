fd0902631e43fffe554cff83ea708d30b72f2988
calico-crusade <holycardboardquest@gmail.com>
2023-09-25 10:32:59 -0400

    Fixing issue associated with default null property handling.

---
+++ src/MangaDexSharp/Helpers/MdCacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace MangaDexSharp;
+
+/// <summary>
+/// Exposes the underlying caching mechanism for Cardboard HTTP tailed to MangaDex
+/// </summary>
+public interface IMdCacheService : ICacheService { }
+
+internal class MdCacheService : DiskCacheService, IMdCacheService
+{
+    public MdCacheService(IMdJsonService json) : base(json) { }
+}
